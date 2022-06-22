8556eae127bd132a1524dc024a0e248457bc4be5
imanuel <toswim100@gmail.com>
2022-06-22 21:55:44 +0300

    feat(swagger): working swagger API + local swagger.json

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import * as swaggerUi from 'swagger-ui-express';
+import swaggerDoc from '../swagger.json'; //option to use the local swagger json file
+
+const app = express();
+const PORT = 3000;
+
+var options = {
+    swaggerOptions: {
+      url: 'http://api.swaggerhub.com/apis/toswim100/calculator/1.0.0?resolved=true'
+    }
+  }
+
+app.use('/api', swaggerUi.serve, swaggerUi.setup(null, options))
+
+app.get('/add/:a&:b', (req, res) => {
+    const c = parseInt(req.params.a) + parseInt(req.params.b)
+    res.send(c.toString())
+});
+app.get('/sub/:a&:b', (req, res) => {
+    const c = parseInt(req.params.a) - parseInt(req.params.b)
+    res.send(c.toString())
+});
+app.get('/div/:a&:b', (req, res) => {
+    if( req.params.b === 0){
+        res.status(400)
+    }
+    const c = parseInt(req.params.a) / parseInt(req.params.b)
+    res.send(c.toString())
+});
+app.get('/mult/:a&:b', (req, res) => {
+    const c = parseInt(req.params.a) * parseInt(req.params.b)
+    res.send(c.toString())
+});
+
+
+app.listen( PORT, () => {
+    console.log(`start listening to port ${PORT}`)
+})
